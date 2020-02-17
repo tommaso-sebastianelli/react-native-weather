aaf2ca951522db615294fd9e9d2316e92af57ae5
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-02-17 18:13:01 +0100

    added nativebase, main page setup

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -1,14 +1,45 @@
-import React, { memo } from 'react'
-import { connect } from 'react-redux'
-import { Text } from 'react-native';
+import { Input, Item, View, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base';
+import React, { memo } from 'react';
+import { StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 
 function Main(props) {
     const { } = props
 
     return (
-        <Text>Main Works!</Text>
+        <View style={style.view}>
+            <Item rounded>
+                <Input placeholder='Rounded Textbox' />
+            </Item>
+            <List style={style.list}>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: 'Image URL' }} />
+                    </Left>
+                    <Body>
+                        <Text>Kumar Pratik</Text>
+                        <Text note>Doing what you like will always keep you happy . .</Text>
+                    </Body>
+                    <Right>
+                        <Text note>3:43 pm</Text>
+                    </Right>
+                </ListItem>
+            </List>
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    view: {
+        padding: 32,
+        justifyContent: 'center',
+        flex: 1
+    },
+    list: {
+        marginTop: 16,
+        left: 0
+    }
+});
 
 const mapStateToProps = (state) => ({
 
