7540c9c50d3af3b52482afbf4e9f1fc51dafa658
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-02-19 12:54:15 +0100

    changed location search mechanism (on submit)

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -1,23 +1,29 @@
-import React, { memo, useState, useEffect, useCallback } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, List, TextInput } from 'react-native-paper';
 import { connect } from 'react-redux';
-import { debounce } from 'lodash';
-import { actions } from '../../redux/search/slice';
 import { dataSelector, loadingSelector } from '../../redux/search/selectors';
-import { TextInput, List, ActivityIndicator, Colors } from 'react-native-paper';
-
+import { actions } from '../../redux/search/slice';
 
 function Main(props) {
     const { dispatchClear, dispatchSearch, loading, result } = props;
     const [searchValue, setSearchValue] = useState('');
 
-    useEffect(() => {
-        if (searchValue.length >= 3) {
+    // useEffect(() => {
+    //     if (searchValue.trim().length >= 3) {
+    //         dispatchSearch(searchValue);
+    //     }else{
+    //         dispatchClear();
+    //     }
+    // }, [searchValue]);
+
+    const onSubmit = () => {
+        if (searchValue.trim().length >= 3) {
             dispatchSearch(searchValue);
-        }else{
+        } else {
             dispatchClear();
         }
-    }, [searchValue]);
+    }
 
     const onChangeText = text => {
         setSearchValue(text);
@@ -30,10 +36,12 @@ function Main(props) {
                 label='Type a location'
                 value={searchValue}
                 onChangeText={text => onChangeText(text)}
-                mode="outlined" />
+                mode="outlined"
+                onSubmitEditing={onSubmit}
+            />
             {(loading) ?
                 <ActivityIndicator animating={true} />
-                : result && result.map(({title, location_type}) => <List.Item
+                : result && result.map(({ title, location_type }) => <List.Item
                     title={title}
                     description={location_type}
                     left={props => <List.Icon icon="map-marker-outline" />}
@@ -62,7 +70,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
     dispatchSearch: (value) => dispatch(actions.start({ searchValue: value })),
-    dispatchClear: ()=> dispatch(actions.clear())
+    dispatchClear: () => dispatch(actions.clear())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(memo(Main))
