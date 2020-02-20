aef090bd9398318cdec729e93a8768c745d4f8d8
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-02-20 11:45:09 +0100

    added pulse effect on main page

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -1,63 +1,76 @@
-import React, { memo, useState, useEffect } from 'react';
-import { Animated, StyleSheet, View } from 'react-native';
+import React, { Component, memo } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { ActivityIndicator, Button, List, TextInput, Title, withTheme } from 'react-native-paper';
 import { connect } from 'react-redux';
 import { dataSelector, loadingSelector } from '../../redux/search/selectors';
 import { actions } from '../../redux/search/slice';
+import Pulse from 'react-native-pulse';
 
-function Main(props) {
-    const { dispatchClear, dispatchSearch, loading, result, theme } = props;
-    const [searchValue, setSearchValue] = useState('')
+class Main extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props);
 
-    const onSubmit = () => {
-        if (searchValue.trim().length >= 3) {
-            dispatchSearch(searchValue);
+        this.state = {
+            searchValue: ''
+        }
+
+    }
+
+    onSubmit = () => {
+        if (this.state.searchValue.trim().length >= 3) {
+            this.props.dispatchSearch(this.state.searchValue);
         } else {
-            dispatchClear();
+            this.props.dispatchClear();
         }
     }
 
-    const onChangeText = text => {
-        setSearchValue(text);
+    onChangeText = text => {
+        this.setState({
+            searchValue: text
+        });
     };
 
-    return (
-        <>
-            <View style={style.title}>
-                <View style={{ flexDirection: 'row' }}>
-                    <Button icon="weather-sunny"></Button>
-                    <Button icon="weather-pouring"></Button>
-                    <Button icon="weather-windy"></Button>
-                    <Button icon="weather-lightning"></Button>
+    render() {
+        return (
+            <>
+                <View style={style.title}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Button icon="weather-sunny"></Button>
+                        <Button icon="weather-pouring"></Button>
+                        <Button icon="weather-windy"></Button>
+                        <Button icon="weather-lightning"></Button>
+                    </View>
+                    <Title style={{ color: this.props.theme.colors.primary }}>
+                        react native weather
+                    </Title>
                 </View>
-                <Title style={{ color: theme.colors.primary }}>
-                    react native weather
-                </Title>
-            </View>
-            <View style={style.searchForm}>
-                <Title>Location</Title>
-                <TextInput
-                    style={style.input}
-                    label='Type a city here'
-                    value={searchValue}
-                    onChangeText={text => onChangeText(text)}
-                    mode="outlined"
-                    onSubmitEditing={onSubmit}
-                />
-                {(loading) ?
-                    <ActivityIndicator animating={true} />
-                    : result && result.map(({ title, location_type }) => <List.Item
-                        title={title}
-                        description={location_type}
-                        left={props => <List.Icon icon="map-marker-outline" />}
-                    />)
-                }
+                <View style={style.searchForm}>
+                    <Title>Location</Title>
+                    <TextInput
+                        style={style.input}
+                        label='Type a city here'
+                        value={this.state.searchValue}
+                        onChangeText={text => this.onChangeText(text)}
+                        mode="outlined"
+                        onSubmitEditing={this.onSubmit}
+                    />
+                    {(this.props.loading) ?
+                        <ActivityIndicator animating={true} />
+                        : this.props.result && this.props.result.map(({ title, location_type }) => <List.Item
+                            title={title}
+                            description={location_type}
+                            left={props => <List.Icon icon="map-marker-outline" />}
+                        />)
+                    }
 
-            </View>
-            <View style={style.footer}>
-            </View>
-        </>
-    )
+                </View>
+                <View style={style.footer}>
+                    <Pulse style={style.pulse} color={this.props.theme.colors.primary} numPulses={4} initialDiameter={500} diameter={800} speed={20} duration={3000} />
+                </View>
+            </>
+        )
+    }
 }
 
 const style = StyleSheet.create({
@@ -69,13 +82,17 @@ const style = StyleSheet.create({
     searchForm: {
         padding: 64,
         // justifyContent: 'center',
-        flex: 0.5
+        flex: 0.3
     },
     input: {
         marginBottom: 16,
     },
-    footer:{
-        flex: 0.2,
+    pulse:{
+        position: 'absolute'
+    },
+    footer: {
+        opacity: 0.4,
+        flex: 0.4,
     }
 });
 
